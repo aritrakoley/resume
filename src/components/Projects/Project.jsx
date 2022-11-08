13f0198717f3c244128eb7ec72247baa87db8158
aritrakoley <koley.aritra824@imerit.net>
2022-11-08 20:08:22 +0530

    Change layout. Projects done. (UX tweaks pending) (WIP #7)

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,23 +2,47 @@ import React from "react";
 
 const Project = ({ url, imgUrl, title, date }) => {
   return (
-    <article>
-      <a href={url} className="image">
-        <img src={imgUrl} alt="" />
-      </a>
-      <div className="inner">
-        <h4>{title}</h4>
-        <p>
-          <em>{date}</em>
-          <br />
-          <em>
-            <a href={url} target="_blank" rel="noreferrer">
+    <div className="bg-slate-700 my-3 px-5 py-5 rounded-3xl">
+      <img
+        className="w-fit object-contain bg-slate-200/70 rounded-3xl h-72 lg:mx-6 lg:w-1/2 lg:h-96"
+        src={imgUrl}
+        alt=""
+      />
+      <div className="mt-8 lg:flex lg:items-center">
+        <h1 className="text-xl font-semibold text-gray-200 capitalize lg:text-4xl">
+          {title}
+        </h1>
+
+        <div className="w-full mt-5 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+          <p className="w-fit max-w-full text-gray-200 text-lg bg-slate-600 rounded-full px-4 mb-1">
+            {date}
+          </p>
+          <a href={url}>
+            <p className="w-fit break-all text-gray-200 text-lg bg-slate-600 rounded-full py-1 px-4">
               {url}
-            </a>
-          </em>
-        </p>
+            </p>
+          </a>
+        </div>
       </div>
-    </article>
+    </div>
+
+    // <div className="flex bg-slate-700 mb-4 rounded-3xl overflow-hidden">
+    //   <a href={url} className="w-[30rem] h-[30rem]overflow-hidden  border border-red-700 ">
+    //     <img src={imgUrl} alt="" className=" aspect-square"/>
+    //   </a>
+    //   <div className="inner">
+    //     <h4>{title}</h4>
+    //     <p>
+    //       <em>{date}</em>
+    //       <br />
+    //       <em>
+    //         <a href={url} target="_blank" rel="noreferrer">
+    //           {url}
+    //         </a>
+    //       </em>
+    //     </p>
+    //   </div>
+    // </div>
   );
 };
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,10 +4,12 @@ const Projects = () => {
   const { title, subtitle, data } = content.projects;
   return (
     <>
-      <div className="container">
-        <h3>{title}</h3>
-        <p>{subtitle}</p>
-        <div className="features">
+      <div className="bg-slate-800 text-slate-200 py-10 px-5 md:px-20 lg:px-40 mt-2 rounded-3xl">
+        <h2 className="text-6xl py-10 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-amber-400 to-orange-600">
+          {title}
+        </h2>
+        <p className="text-xl text-slate-400">{subtitle}</p>
+        <div className="my-10">
           {data.map((e) => (
             <Project key={e.url} {...e} />
           ))}
